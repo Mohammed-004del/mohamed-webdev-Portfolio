12522e24f8c0329ff8cd35de5342c79b722d9602
Mohammed-004del <MohammedMahmoodHamed004@gmail.com>
2025-12-13 17:45:43 +0200

    Editing Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,6 +7,7 @@ import project5 from "./images/project5.png";
 import project6 from "./images/project6.png";
 import project7 from "./images/project7.png";
 import project8 from "./images/project8.png";
+import project9 from "./images/project9.png";
 
 
 const Projects = () => {
@@ -74,6 +75,14 @@ const Projects = () => {
       image: project8,
       live: "https://mohammed-004del.github.io/User-Gallery-project/",
       code: "https://github.com/Mohammed-004del/User-Gallery-project/blob/main/src/index.html",
+    },
+    {
+      title: "AL-Araby Oils E-commerce website",
+      description: "A fully responsive e-commerce frontend web application for natural oils products. Built using React, TypeScript, and Tailwind CSS with a strong focus on clean UI, smooth UX, and scalable component-based architecture. The application includes product listings, cart functionality, modern navigation, and optimized performance for an intuitive shopping experience.",
+      tags: ["React", "TypeScript", "Tailwind CSS", "Responsive Design", "E-commerce", "UI/UX"],
+      image: project8,
+      live: "https://mohammed-004del.github.io/AL-Araby-Oils-E-commerce-Website/",
+      code: "https://github.com/Mohammed-004del/AL-Araby-Oils-E-commerce-Website/blob/main/index.html",
     },
   ];
 
